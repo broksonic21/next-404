62a481d907d665ce302396932729e7e2ed00007e
broksonic21 <24306571+broksonic21@users.noreply.github.com>
2023-09-29 16:02:03 -0400

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  /**
+   * Just an example of middleware
+   * do nothing
+   */
+}
+
+export const config = {
+    matcher: '/somerandompath',
+}
